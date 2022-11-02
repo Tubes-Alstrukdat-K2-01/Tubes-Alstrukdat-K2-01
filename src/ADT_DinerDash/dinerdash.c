8debe2ff144ed77a4a7892e1355abced43bb6281
Muhammad Thoriq Ramadhan <98358997+mtr06@users.noreply.github.com>
2022-11-02 11:46:36 +0700

    Diner_Dash

---
+++ src/ADT_DinerDash/dinerdash.c
@@ -0,0 +1,254 @@
+#include "dinerdash.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+boolean gameover(Queue q, int pelayanan){
+    return ((length(q) > 7) || pelayanan == 15);
+}
+/*
+    I.S : q dan pelanggaran sembarang
+    F.S : True jika panjang antrian queue lebih dari 7 atau pelayanan mencapai 15
+          False jika belum termasuk katergori di atas
+*/
+
+void displayPesanan(Queue q){
+    printf("Daftar Pesanan\n");
+    printf("Memasak | Durasi memasak | Ketahanan | Harga\n");
+    printf("--------------------------------------------\n");
+    if(isEmpty(q)){
+        printf("        |                |           |      \n");
+    }
+    else{
+        int i;
+        if (IDX_TAIL(q) >= IDX_HEAD(q)){
+            for(i=IDX_HEAD(q) ; i <= IDX_TAIL(q) ; i++){
+                printf("M%d     | %d             | %d        | %d\n",q.buffer[i].makanan, q.buffer[i].durasi, q.buffer[i].ketahanan, q.buffer[i].harga);
+            }
+        }
+        else {
+            for(i=IDX_HEAD(q) ; i <= CAPACITY+IDX_TAIL(q) ; i++){
+                printf("M%d     | %d             | %d        | %d\n",q.buffer[i%CAPACITY].makanan, q.buffer[i%CAPACITY].durasi, q.buffer[i%CAPACITY].ketahanan, q.buffer[i%CAPACITY].harga);
+            }
+        }
+    }
+}
+/*
+    I.S : q sembarang
+    F.S : Menampilkan Pesanan yang terdiri dari Makanan, Durasi, Ketahanan, dan Harga
+*/
+
+void displayMemasak(Queue q){
+    printf("Daftar Makanan yang sedang dimasak\n");
+    printf("Memasak | Sisa durasi memasak \n");
+    printf("------------------------------\n");
+    if(isEmpty(q)){
+        printf("        |                \n");
+    }
+    else{
+        int i;
+        if (IDX_TAIL(q) >= IDX_HEAD(q)){
+            for(i=IDX_HEAD(q) ; i <= IDX_TAIL(q) ; i++){
+                if(q.buffer[i].durasi != 0){
+                    printf("M%d     | %d                \n",q.buffer[i].makanan, q.buffer[i].durasi);
+                }
+            }
+        }
+        else {
+            for(i=IDX_HEAD(q) ; i <= CAPACITY+IDX_TAIL(q) ; i++){
+                if(q.buffer[i%CAPACITY].durasi != 0){
+                    printf("M%d     | %d                \n",q.buffer[i%CAPACITY].makanan, q.buffer[i%CAPACITY].durasi);
+                }
+            }
+        }
+    }
+}
+/*
+    I.S : q sembarang
+    F.S : Menampilkan Daftar Pesanan yang sedang dimasak yang terdiri dari Makanan dan Durasi
+*/
+
+void displayDisajikan(Queue q){
+    printf("Daftar Makanan yang sedang dimasak\n");
+    printf("Memasak | Sisa ketahanan memasak \n");
+    printf("------------------------------\n");
+    if(isEmpty(q)){
+        printf("        |                \n");
+    }
+    else{
+        int i;
+        if (IDX_TAIL(q) >= IDX_HEAD(q)){
+            for(i=IDX_HEAD(q) ; i <= IDX_TAIL(q) ; i++){
+                if(q.buffer[i].durasi == 0){
+                    printf("M%d     | %d                \n",q.buffer[i].makanan, q.buffer[i].ketahanan);
+                }
+            }
+        }
+        else {
+            for(i=IDX_HEAD(q) ; i <= CAPACITY+IDX_TAIL(q) ; i++){
+                if(q.buffer[i%CAPACITY].durasi == 0){
+                    printf("M%d     | %d                \n",q.buffer[i%CAPACITY].makanan, q.buffer[i%CAPACITY].ketahanan);
+                }
+            }
+        }
+    }
+}
+/*
+    I.S : q sembarang
+    F.S : Menampilkan Daftar Masakan yang sudah bisa disajikan yang sedang dimasak yang terdiri dari Makanan dan Ketahanan
+*/
+void cook(Queue qpesan, Queue *qmasak, int makanan){
+    if(isMember(qpesan,makanan)){
+        if(isMember(*qmasak,makanan)){
+            printf("Makanan sedang atau sudah dimasak!\n");
+        }
+        else{
+            int durasi,ketahaan,harga;
+            dequeueMember(&qpesan, &makanan, &durasi, &ketahaan, &harga);
+            enqueue(qmasak,makanan,durasi,ketahaan,harga);
+            printf("Berhasil memasak M%d\n",makanan);
+        }
+    }
+    else{
+        printf("Makanan tidak ada dalam antrian pesanan!\n");
+    }
+}
+/*
+    I.S : Input bebas
+    F.S : Memasukkan makanan kedalam qmasak
+*/
+
+void serve(Queue *qpesan, Queue *qmasak, int makanan, int *pelayanan, int *saldo){
+    if(isMember(*qmasak,makanan)){
+        int idx;
+        boolean found = false;
+        if (IDX_TAIL(*qmasak) >= IDX_HEAD(*qmasak)){
+            idx = IDX_HEAD(*qmasak);
+            while((idx <= IDX_TAIL(*qmasak)) && (!found)){
+                if((*qmasak).buffer[idx].makanan == makanan){
+                    found = true;
+                }
+                else{
+                    idx = (idx+1)%CAPACITY;
+                }
+            }
+        }
+        else {
+            int i = IDX_HEAD(*qmasak);
+            while((i < CAPACITY) && (!found)){
+                if((*qmasak).buffer[i].makanan == makanan){
+                    found = true;
+                    idx = i;
+                }
+                else{
+                    i = (i+1)%CAPACITY;
+                }
+            }
+            i = 0;
+            while((i <= IDX_TAIL(*qmasak)) && (!found)){
+                if((*qmasak).buffer[i].makanan == makanan){
+                    found = true;
+                    idx = i;
+                }
+                else{
+                    i = (i+1)%CAPACITY;
+                }
+            }
+        }
+        if((*qmasak).buffer[idx].durasi == 0){
+            if(HEAD_makanan(*qpesan) == makanan){
+                int durasi, ketahanan, harga;
+                dequeue(qpesan,&makanan,&durasi,&ketahanan,&harga);
+                *saldo += harga;
+                dequeueMember(qmasak,&makanan,&durasi,&ketahanan,&harga);
+                *pelayanan++;
+            }
+            else{
+                printf("M%d belum dapat disajikan karena M%d belum selesai\n",makanan,HEAD_makanan(*qpesan));
+            }
+        }
+        else{
+            printf("Makanan belum selesai dimasak!\n");
+        }
+    }
+    else{
+        printf("Makanan belum dimasak atau tidak ada dalam pesanan\n");
+    }
+}
+/*
+    I.S : Input bebas
+    F.S : Jika makanan adalah head qpesan maka makanan akan dihapus dari qmasak dan pelayanan diincrement 1
+          Jika tidak maka output "makanan" belum dapat disajikan karena "head qpesan" belum selesai
+*/
+
+void turn(Queue *q){
+    int i;
+    if (IDX_TAIL(*q) >= IDX_HEAD(*q)){
+        for(i=IDX_HEAD(*q) ; i <= IDX_TAIL(*q) ; i++){
+            if((*q).buffer[i].durasi == 0){
+                (*q).buffer[i].ketahanan--;
+            }
+            else{
+                (*q).buffer[i].durasi--;
+            }
+        }
+    }
+    else {
+        for(i=IDX_HEAD(*q) ; i <= CAPACITY+IDX_TAIL(*q) ; i++){
+            if((*q).buffer[i%CAPACITY].durasi == 0){
+                (*q).buffer[i%CAPACITY].ketahanan--;
+            }
+            else{
+                (*q).buffer[i%CAPACITY].durasi--;
+            }
+        }
+    }
+}
+/*
+    I.S : qmasak sembarang
+    F.S : Melakukan decrement pada durasi yang tidak 0 dan melakukan decrement pada ketahanan jika durasi == 0
+*/
+
+void dinerdash(){
+    Queue qpesan, qmasak;
+    CreateQueue(&qpesan); CreateQueue(&qmasak);
+    int makanan=0, durasi, ketahanan, harga;
+    for(makanan; makanan < 3; makanan++){
+        durasi = rand()%5 + 1;
+        ketahanan = rand()%5 + 1;
+        harga = (rand()%41 + 10)*1000;
+        enqueue(&qpesan, makanan, durasi, ketahanan, harga);
+    }
+    int pelayanan = 0, saldo = 0;
+    printf("Selamat Datang di Diner Dash!\n\n");
+    while(!gameover(qpesan,pelayanan)){
+        printf("SALDO: %d\n\n",saldo);
+        displayPesanan(qpesan);
+        printf("\n");
+        displayMemasak(qmasak);
+        printf("\n");
+        displayDisajikan(qmasak);
+        printf("\n");
+        char command[10];
+        int m;
+        printf("MASUKKAN COMMAND: ");
+        scanf(" %s M%d",&command,&m);
+        turn(&qmasak);
+        if(command[0] == 'C' && command[1] == 'O' && command[2] == 'O' && command[3] == 'K'){
+            cook(qpesan,&qmasak,m);
+        }
+        else if(command[0] == 'S' && command[1] == 'E' && command[2] == 'R' && command[3] == 'V' && command[4] == 'E'){
+            serve(&qpesan,&qmasak,m,&pelayanan,&saldo);
+        }
+        else{
+            printf("Invalid COMMAND!\n");
+        }
+        durasi = rand()%5 + 1;
+        ketahanan = rand()%5 + 1;
+        harga = (rand()%41 + 10)*1000;
+        enqueue(&qpesan, makanan, durasi, ketahanan, harga);
+        makanan++;
+        printf("==========================================================\n\n");
+    }
+    printf("Game Over!, SALDO ANDA: %d\n",saldo);
+}
---
+++ src/ADT_DinerDash/queue.h
@@ -0,0 +1,82 @@
+/* File : queue.h */
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi statik */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "boolean.h"
+
+#define IDX_UNDEF -1
+#define CAPACITY 10
+
+/* Definisi elemen dan address */
+typedef struct {
+        int makanan;
+        int durasi; //dari 1-5
+        int ketahanan; //dari 1-5
+        int harga; //dari 10000-50000 dengan kelipatan 1000
+} ElType;
+
+typedef struct {
+	ElType buffer[CAPACITY]; 
+	int idxHead;
+	int idxTail;
+} Queue;
+
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah Queue, maka akses elemen : */
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define     HEAD_makanan(q) (q).buffer[(q).idxHead].makanan
+#define     HEAD_durasi(q) (q).buffer[(q).idxHead].durasi
+#define     HEAD_ketahanan(q) (q).buffer[(q).idxHead].ketahanan
+#define     HEAD_harga(q) (q).buffer[(q).idxHead].harga
+#define     TAIL_makanan(q) (q).buffer[(q).idxTail].makanan
+#define     TAIL_durasi(q) (q).buffer[(q).idxTail].durasi
+#define     TAIL_ketahanan(q) (q).buffer[(q).idxTail].ketahanan
+#define     TAIL_harga(q) (q).buffer[(q).idxTail].harga
+
+/* *** Kreator *** */
+void CreateQueue(Queue *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmpty(Queue q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFull(Queue q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+
+int length(Queue q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueue(Queue *q, int makanan,int durasi ,int ketahanan, int harga);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+
+void dequeue(Queue *q, int *makanan,int *durasi, int *ketahanan, int *harga);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+boolean isMember(Queue q, int makanan);
+/*
+   I.S : q dan makanan sembarangan
+   F.S : True jika makanan ada di dalam queue
+*/
+
+void dequeueMember(Queue *q, int *makanan, int *durasi, int *ketahanan, int *harga);
+/*
+   I.S : q dan makanan sembarangan, makanan bisa bukan anggota dari q
+   F.S : q dengan makanan telah dihapus
+*/
+
+#endif
