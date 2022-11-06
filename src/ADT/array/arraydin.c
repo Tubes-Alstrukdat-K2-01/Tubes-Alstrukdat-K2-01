f6e624a63c898127ea81626340fed0f71ea82019
hitsutakarin77@gmail.com <hitsutakarin77@gmail.com>
2022-11-06 11:25:14 +0700

    Push ADT

---
+++ src/ADT/array/arraydin.c
@@ -0,0 +1,95 @@
+#include "arraydin.h"
+#include <stdlib.h>
+
+/**
+ * Konstruktor
+ * I.S. sembarang
+ * F.S. Terbentuk ArrayDin kosong dengan ukuran InitialSize
+ */
+ArrayDin MakeArrayDin(){
+    ArrayDin p;
+    p.A = (ElType*) malloc (InitialSize*sizeof(ElType));
+    p.Capacity = InitialSize;
+    p.Neff = 0;
+    return p;
+}
+
+/**
+ * Destruktor
+ * I.S. ArrayDin terdefinisi
+ * F.S. array->A terdealokasi
+ */
+void DeallocateArrayDin(ArrayDin *array){
+    free((*array).A);
+    (*array).Capacity = 0;
+    (*array).Neff = 0;
+}
+
+/**
+ * Fungsi untuk mengetahui apakah suatu array kosong.
+ * Prekondisi: array terdefinisi
+ */
+boolean IsEmpty(ArrayDin array){
+    return (array.Neff == 0);
+}
+
+/**
+ * Fungsi untuk mendapatkan banyaknya elemen efektif array, 0 jika tabel kosong.
+ * Prekondisi: array terdefinisi
+ */
+int Length(ArrayDin array){
+    return array.Neff;
+}
+
+/**
+ * Mengembalikan elemen array L yang ke-I (indeks lojik).
+ * Prekondisi: array tidak kosong, i di antara 0..Length(array).
+ */
+ElType Get(ArrayDin array, IdxType i){
+    return array.A[i];
+}
+
+/**
+ * Fungsi untuk mendapatkan kapasitas yang tersedia.
+ * Prekondisi: array terdefinisi
+ */
+int GetCapacity(ArrayDin array){
+    return array.Capacity;
+}
+
+/**
+ * Fungsi untuk menambahkan elemen baru di index ke-i
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+void InsertAt(ArrayDin *array, ElType el, IdxType i){
+    if ((*array).Neff == (*array).Capacity){
+        ElType *list = (ElType*) malloc (((*array).Capacity*2)*sizeof(ElType));
+        IdxType p=0;
+        for (p; p<(*array).Neff; p++){
+            list[p] = Get(*array , p);
+        }
+        free((*array).A);
+        (*array).A = list;
+        (*array).Capacity *= 2;
+    }
+    IdxType j=(*array).Neff;
+    for (j;j>i;j--){
+        (*array).A[j]=(*array).A[j-1];
+    }
+    (*array).A[i]=el;
+    (*array).Neff+=1;
+}
+
+/**
+ * Fungsi untuk menghapus elemen di index ke-i ArrayDin
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+
+void DeleteAt(ArrayDin *array, IdxType i){
+    IdxType j = i;
+    while (j<(*array).Neff-1){
+        (*array).A[j] = (*array).A[j+1];
+        j++;
+    }
+    array->Neff--;
+}
